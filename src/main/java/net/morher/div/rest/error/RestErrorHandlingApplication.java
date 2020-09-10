9c59d1bcd838d988c919a556f26f27df53d84bab
Morten Hermansen <post@morher.net>
2020-09-10 14:27:49 +0200

    Initial commit

---
+++ src/main/java/net/morher/div/rest/error/RestErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package net.morher.div.rest.error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestErrorHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/morher/div/rest/error/RestErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.morher.div.rest.error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
